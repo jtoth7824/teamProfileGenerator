ec2cf586230d771bfd241d0807acc5de9b6ffe77
LostInSpace <71476446+jtoth7824@users.noreply.github.com>
2021-01-20 16:43:50 -0500

    added constructor Employee and tests passed

--- Develop/lib/Employee.js
+++ Develop/lib/Employee.js
@@ -1,1 +1,20 @@
 // TODO: Write code to define and export the Employee class
+function Employee(name, id, email) {
+    this.name = name;
+    this.id = id;
+    this.email = email;
+    this.getName = () => {
+        return this.name;
+      }
+    this.getId = () => {
+        return this.id;
+    }
+    this.getEmail = () => {
+        return this.email;
+    }
+    this.getRole = () => {
+        return "Employee";
+    }
+}
+
+module.exports = Employee;
