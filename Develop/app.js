d661d8a61504229ebf0e5f02e4258154baef94c2
LostInSpace <71476446+jtoth7824@users.noreply.github.com>
2021-01-27 13:32:33 -0500

    added comments to css file

--- Develop/app.js
+++ Develop/app.js
@@ -10,7 +10,6 @@ const OUTPUT_DIR = path.resolve(__dirname, "output");
 const outputPath = path.join(OUTPUT_DIR, "team.html");
 
 const render = require("./lib/htmlRenderer");
-const Employee = require("./lib/Employee");
 
 const Employees = [];
 var renderedPage;
